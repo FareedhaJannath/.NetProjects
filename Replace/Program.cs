0930d8da7d1429bbd586b6237323532daeb56d18
Mohamed <142555@cognizant.com>
2019-04-29 16:46:04 +0530

    DotNet Project

---
+++ Replace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "please contact the \nadministrator";
+            Console.WriteLine(input);
+            string output = input.Replace('\n', ' ');
+            Console.WriteLine(output);
+            Console.Read();
+        }
+    }
+}
